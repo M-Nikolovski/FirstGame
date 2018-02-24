2adfb6dc087c5b823b4ff8b63fdffbe9a041fc55
HristinaHihi <35847731+HristinaHihi@users.noreply.github.com>
2018-02-24 06:57:41 +0100

    Start menu v2

---
+++ Assets/macedoniaplayer.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using UnityEngine.SceneManagement; // <<<<<< ADD THIS.
+
+public class macedonianplayer : MonoBehaviour
+{
+
+    // Use this for initialization
+    public void changeToScene(int Level01)
+    {
+        SceneManager.LoadScene(Level01);
+
+    }
+}
