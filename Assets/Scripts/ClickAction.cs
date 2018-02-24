adb3b7670f009397a4ec05fbb2bb310301fcfe50
Mirko Nikolovski <nikolovski-m@hotmail.com>
2018-02-24 16:15:42 +0100

    Scripts for menu and scene select

---
+++ Assets/Scripts/ClickAction.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ClickAction : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+	    if (Input.GetMouseButtonDown(0))
+	    {
+	        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+	        RaycastHit hit;
+
+	        if (Physics.Raycast(ray, out hit))
+	        {
+	            if (hit.transform.name.ToLower().Equals("macedonia"))
+	            {
+	                SceneManager.LoadScene("MacedoniaPlayground");
+	            }
+	            else if (hit.transform.name.ToLower().Equals("germany"))
+	            {
+	                SceneManager.LoadScene("GermanyPlayground");
+	            }
+	            else if (hit.transform.name.ToLower().Equals("china"))
+	            {
+	                SceneManager.LoadScene("ChinaPlayground");
+	            }
+            }
+
+        }
+
+	    
+
+
+	}
+}
---
+++ Assets/Scripts/CountrySelector.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+using Button = UnityEngine.Experimental.UIElements.Button;
+
+public class CountrySelector : MonoBehaviour
+{
+    private GameObject Macedonia;
+    private GameObject Germany;
+    private GameObject China;
+
+	// Use this for initialization
+	void Start () {
+	    Macedonia = GameObject.Find("MacedoniaPlayground");
+        Germany = GameObject.Find("GermanyPlayground");
+        China = GameObject.Find("ChinaPlayground");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void ButtonClick(Button button)
+    {
+        if (button.name == "Macedonia")
+        {
+            SceneManager.LoadScene("MacedoniaPlayground");
+        }
+    }
+}
